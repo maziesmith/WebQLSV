8c09cd9bc5b653ac4af16432056fd2cef0e27974
d3lt4-024 <phamhaivu2410@gmail.com>
2020-02-09 00:56:39 +0700

    List of student user

---
+++ teacher/students.php
@@ -0,0 +1,99 @@
+<?php
+
+ob_start();
+session_start();
+
+//check if session is not valid
+if (!isset($_SESSION['user_id'])) {
+    header('location: ../login.php');
+}
+?>
+<?php include('../connect.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Online Attendance Management System 1.0</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+    <link rel="stylesheet" href="styles.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+</head>
+<body>
+
+<header>
+
+    <h1>Online Attendance Management System 1.0</h1>
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="students.php">Students</a>
+        <a href="teachers.php">Faculties</a>
+        <a href="attendance.php">Attendance</a>
+        <a href="editProfile.php">Edit Profile</a>
+        <a href="editProfileStudent.php">Edit Profile Student</a>
+        <a href="../logout.php">Logout</a>
+
+
+    </div>
+
+</header>
+
+<center>
+
+    <div class="row">
+
+        <div class="content">
+            <h3>Student List</h3>
+            <br>
+            </form>
+            <br>
+            <table class="table table-stripped">
+                <thead>
+                <tr>
+                    <th scope="col">ID.</th>
+                    <th scope="col">Họ Tên</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Số Điện Thoại</th>
+                    <th scope="col">GPA</th>
+                </tr>
+                </thead>
+
+                <?php
+                $result = GetAllSV(); //get all SinhVien in SinhVien table
+                foreach ($result as $key => $value) {
+                    ?>
+                    <tbody>
+                    <tr>
+                        <td><?php echo $value['Id']; ?></td>
+                        <td><?php echo $value['HoTen']; ?></td>
+                        <td><?php echo $value['Email']; ?></td>
+                        <td><?php echo $value['SĐT']; ?></td>
+                        <td><?php echo $value['GPA']; ?></td>
+                    </tr>
+                    </tbody>
+
+                    <?php
+                }
+                ?>
+
+            </table>
+
+        </div>
+
+    </div>
+
+</center>
+
+</body>
+</html>
